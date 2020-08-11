2d9988b3109be805c227af4eba29262e74025b4e
AxSch <alex_schuneman@outlook.com>
2020-08-11 15:12:27 +0100

    Configure tailwind css & google fonts

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    theme: {
+        extend: {
+            backgroundColor: theme => ({
+                'primary': 'rgb(0, 43, 128)',
+                'secondary': 'rgba(0, 43, 128, 0.05)',
+                'accent': 'rgb(45, 185, 178)'
+            }),
+            fontFamily: {
+                'display': ['Patua One'],
+                'sans-serif': ['Open Sans'],
+            },
+        }
+    },
+}
